b6b93dc9e456580e66283c85dedc5210e5f0f1a3
march <hbuw75@163.com>
2023-03-20 22:14:25 +0800

    320

---
+++ 320/320.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	/*int b = a++;*///后置++，表示b先等于a，然后a在+1；10
+//	int b = ++a;//前置++，表示a先+1，再等于b；11
+//	printf("%d\n",b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//强制转换类型
+//	int a = (int)3.14;//由于3.14为双精度的浮点数，需要使用double类型来表示，用int表示时会导致
+//	//3.14去掉小数点后的数字，输出结果为3.使用（int）后可以强制转换类型。
+//
+//	printf("%d\n", a);
+//	return 0;
+//}
+
