5da3547857d361dbd51155d452dd1df773060823
march <hbuw75@163.com>
2023-03-31 00:46:48 +0800

    do while,阶乘，二分法问题

--- 330/330.cpp
+++ 330/330.cpp
@@ -14,10 +14,86 @@
 //	return 0;
 //}
 
+//阶乘计算器；
+//int main()
+//{
+//	int i = 0;
+//	printf("please entering the number of 'n':");
+//	scanf("%d", &i);
+//	long int j = 0;
+//	for (j = 1; i > 0; i--)
+//	{
+//		j = j * i;
+//	}
+//	printf("%d", j);
+//	return 0;
+//}
+
+//问：1!+2!+3!+...+10!=，
+// 解法1：
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 0;
+//	int sum = 0;
+//	for (i = 1; i < 11; i++)
+//	{
+//		ret = 1;
+//		for (n = 1; n <= i; n++)
+//		{
+//			ret *= n;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//解法2：
+//int main()
+//{
+//	int n = 0;
+//	int sum = 0;
+//	int ret = 1;
+//	for (n = 1; n < 11; n++)
+//	{
+//		ret *= n;
+//		sum += ret;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//二分查找
 int main()
 {
-	int i = 0;
-	printf("please entering the number of 'n':\n");
-	scanf("%d", &i);
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int left = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int right =sz-1 ;
+	int mid = 0;
+	int numb = 0;
+	printf("please the number(the number not over ten):");
+	scanf("%d", &numb);
+	while (left<=right)
+	{
+		mid = (left + right) / 2;
+		if (arr[mid]<numb)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > numb)
+		{
+			right = mid - 1;
+		}
+		else if(arr[mid] = numb)
+		{
+			printf("fond it,the arr is:%d\n",mid);
+			break;
+		}
+	}
+	if (left > right)
+		printf("not find");
 	return 0;
 }
