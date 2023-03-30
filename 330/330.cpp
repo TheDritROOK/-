1c87f8ac6335bab6e7f56e64f0cf3518b9badf4c
march <hbuw75@163.com>
2023-03-30 22:16:54 +0800

    do while

---
+++ 330/330.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i == 5)
+//			continue;//1234死循环
+//			//break;//1234跳出循环。
+//		printf("%d ", i);
+//		i++;
+//	} while (i < 11);//do while，while在大括号后面，需要加上分号；
+//	return 0;
+//}
+
+int main()
+{
+	int i = 0;
+	printf("please entering the number of 'n':\n");
+	scanf("%d", &i);
+	return 0;
+}
