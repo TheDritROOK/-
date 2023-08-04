7e31ad224001f9dac71b78fe96f288966f222eeb
march <hbuw75@163.com>
2023-08-05 00:48:09 +0800

    84/

--- 331/331.cpp
+++ 331/331.cpp
@@ -23,3 +23,16 @@
 //	return 0;
 //}
 
+using namespace System;
+using namespace System::Threading;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console::WriteLine("Sleep for 2 seconds.");
+        Thread::Sleep(2000);
+    }
+
+    Console::WriteLine("Main thread exits.");
+}
